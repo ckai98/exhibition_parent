f634915d5f6307b9ff3373531455f2ee79f94632
ChrisCK1998 <ckai98@qq.com>
2021-05-12 02:28:57 +0800

    添加数字字典模块

--- service/service_dict/src/main/java/com/chenkai/exhibition/dict/controller/DictController.java
+++ service/service_dict/src/main/java/com/chenkai/exhibition/dict/controller/DictController.java
@@ -3,12 +3,10 @@ package com.chenkai.exhibition.dict.controller;
 import com.chenkai.exhibition.common.result.Result;
 import com.chenkai.exhibition.dict.service.DictService;
 import com.chenkai.exhibition.model.dict.Dict;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,9 +14,10 @@ import java.util.List;
  * @author Kai Chen
  * @create 2021-05-12-0:32
  */
-
+@Api(tags = "数据字典接口")
 @RestController
 @RequestMapping("admin/dict")
+@CrossOrigin
 public class DictController {
     @Autowired
     private DictService dictService;
